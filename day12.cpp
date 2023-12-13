4d81ac3942f2b8eeac53b6038dfd708e46382e58
Alexander Festini <afestini@gmail.com>
2023-12-13 21:25:41 +0100

    Day 12 optimizations

--- day12.cpp
+++ day12.cpp
@@ -34,29 +34,24 @@ static vector<Line> ReadMap() {
 
 
 static int64_t Solve(string_view pattern, span<const int64_t> groups, Cache& cache) {
-	if (groups.empty())
-		return (ranges::count(pattern, '#') == 0) ? 1 : 0;
+	while (pattern.starts_with('.')) pattern.remove_prefix(1);
 
-	if (pattern.empty())
-		return groups.empty() ? 1 : 0;
+	if (groups.empty())
+		return (ranges::contains(pattern, '#')) ? 0 : 1;
+
+	if (pattern.empty()) return 0;
 
 	auto [entry, added] = cache.try_emplace(pattern.size() << 32 | groups.size());
 	if (!added)
 		return entry->second;
 
-	int64_t sub1 = 0;
-	if (".?"s.contains(pattern[0])) {
-		sub1 = Solve(pattern.substr(1), groups, cache);
-	}
+	const int64_t sub1 = (pattern[0] == '?') ? Solve(pattern.substr(1), groups, cache) : 0;
 
 	int64_t sub2 = 0;
-	if ("#?"s.contains(pattern[0])) {
-		if (ssize(pattern) < groups[0]) sub2 = 0;
-		else if (pattern.substr(0, groups[0]).contains('.')) sub2 = 0;
-		else if (ssize(pattern) == groups[0]) sub2 = (groups.size() == 1) ? 1 : 0;
-		else if (pattern[groups[0]] == '#') sub2 = 0;
-		else sub2 = Solve(pattern.substr(groups[0] + 1), groups.subspan(1), cache);
-	}
+	if (pattern.substr(0, groups[0]).contains('.')) sub2 = 0;
+	else if (ssize(pattern) == groups[0] && groups.size() == 1) sub2 = 1;
+	else if (ssize(pattern) > groups[0] && pattern[groups[0]] != '#')
+		sub2 = Solve(pattern.substr(groups[0] + 1), groups.subspan(1), cache);
 
 	return entry->second = sub1 + sub2;
 }
@@ -81,18 +76,18 @@ export void day12_1() {
 export void day12_2() {
 	const auto start = chrono::high_resolution_clock::now();
 
-	int64_t sum = 0;
 	auto gears = ReadMap();
 
-	for (auto& g : gears) {
+	for_each(execution::par_unseq, gears.begin(), gears.end(), [](auto& g) {
 		g.pattern = views::repeat(g.pattern, 5) | views::join_with('?') | ranges::to<string>();
 		g.groups = views::repeat(g.groups, 5) | views::join | ranges::to<vector>();
-	}
+	});
 
-	for (const auto& g : gears) {
-		Cache cache;
-		sum += Solve(g.pattern, g.groups, cache);
-	}
+	const int64_t sum = transform_reduce(execution::par_unseq, gears.begin(), gears.end(), 0LL, plus<>(),
+		[](const auto& g) {
+			Cache cache;
+			return Solve(g.pattern, g.groups, cache);
+		});
 
 	const auto duration = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now() - start);
 	println("Day 12b: {} ({})", sum, duration);
