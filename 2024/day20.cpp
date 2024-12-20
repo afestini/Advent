6d18a546a5437cc640ed796b144db0040c8e764d
Alexander Festini <afestini@gmail.com>
2024-12-20 13:03:26 +0100

    Optimize Day 20

--- 2024/day20.cpp
+++ 2024/day20.cpp
@@ -44,9 +44,8 @@ export void day20_1() {
 
 	Map2D map("day20.txt");
 
-	vector<int64_t> costs_data(map.width*map.height);
+	vector<int64_t> costs_data(map.width*map.height, -1);
 	mdspan<int64_t, extents<size_t, dynamic_extent, dynamic_extent>, layout_left> costs(costs_data.data(), map.width, map.height);
-	ranges::fill(costs_data, -1);
 
 	fill_cost(map, costs);
 
@@ -72,46 +71,40 @@ export void day20_1() {
 }
 
 
-static vector<Vec2i> find_path(const Map2D& map) {
-	Vec2i prev_pos {-1,-1};
-	Vec2i pos = get_start(map);
-
-	vector<Vec2i> path {pos};
-
-	while (map[pos] != 'E') {
-		for (const auto dir : directions) {
-			const auto next_pos = pos + dir;
-			if (next_pos != prev_pos && (map[next_pos] == '.' || map[next_pos] == 'E')) {
-				prev_pos = pos;
-				pos = next_pos;
-				path.emplace_back(next_pos);
-				break;
-			}
-		}
-	}
-	return path;
-}
-
-
 export void day20_2() {
 	const auto start_time = high_resolution_clock::now();
 
 	Map2D map("day20.txt");
 
-	const auto path = find_path(map);
-
 	const int64_t cheat_time = 20;
 	const int64_t threshold = 100;
+
+	vector<int64_t> costs_data(map.width * map.height, -1);
+	mdspan<int64_t, extents<size_t, dynamic_extent, dynamic_extent>, layout_left> costs(costs_data.data(), map.width, map.height);
+
+	fill_cost(map, costs);
+
 	int64_t cheat_count = 0;
 
-	for (size_t i = 0; i < path.size(); ++i) {
-		for (size_t k = i + threshold; k < path.size(); ++k) {
-			Vec2i from = path[i];
-			Vec2i to = path[k];
-			const auto distance = abs(from.x - to.x) + abs(from.y - to.y);
-			const auto saved = k - i - distance;
-			if (saved >= threshold && distance <= cheat_time)
-				++cheat_count;
+	for (Vec2i from {0,1}; from.y < map.height - 1; ++from.y) {
+		for (from.x = 1; from.x < map.width - 1; ++from.x) {
+			const auto from_cost = costs[from.x, from.y];
+			if (from_cost < 0) continue;
+
+			for (Vec2i to {0,from.y - cheat_time}; to.y <= from.y + cheat_time; ++to.y) {
+				const auto dy = abs(to.y - from.y);
+				for (to.x = from.x - cheat_time + dy; to.x <= from.x + cheat_time - dy; ++to.x) {
+					if (!map.is_in_bounds(to)) continue;
+
+					const auto to_cost = costs[to.x, to.y];
+					if (to_cost < 0) continue;
+
+					const auto distance = abs(from.x - to.x) + dy;
+					const auto saved = to_cost - from_cost - distance;
+					if (saved >= threshold && distance <= cheat_time)
+						++cheat_count;
+				}
+			}
 		}
 	}
 
