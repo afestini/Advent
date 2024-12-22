c27dc3b95fa588aefb2a174bb75326e24bed7d83
Alexander Festini <afestini@gmail.com>
2024-12-22 09:22:54 +0100

    Day 22 optimized key

--- 2024/day22.cpp
+++ 2024/day22.cpp
@@ -24,7 +24,6 @@ static int64_t& mix(int64_t& secret, int64_t value) { return secret ^= value; }
 static void prune(int64_t& secret) { secret &= 0xFFFFFF; }
 
 
-
 export void day22_1() {
 	const auto start_time = high_resolution_clock::now();
 
@@ -46,32 +45,31 @@ export void day22_1() {
 export void day22_2() {
 	const auto start_time = high_resolution_clock::now();
 
-	struct Hash {
-		size_t operator()(array<int16_t, 4> a) const { return bit_cast<size_t>(a); }
-	};
-	unordered_map<array<int16_t, 4>, int64_t, Hash> global_prices;
+	vector<int64_t> global_prices(1 << 20);
 
 	for (auto secret : file_input()) {
-		int64_t prev_price = secret % 10;
-		array<int16_t, 4> sequence = {};
-		unordered_set<array<int16_t, 4>, Hash> seen;
+		int8_t prev_price = secret % 10;
+		uint32_t sequence = 0;
+		vector<bool> seen(1 << 20);
 
 		for (int i = 1; i <= 2000; ++i) {
 			secret = (secret ^ (secret << 6)) & 0xFFFFFF;
 			secret = (secret ^ (secret >> 5)) & 0xFFFFFF;
 			secret = (secret ^ (secret << 11)) & 0xFFFFFF;
-			const auto price = secret % 10;
+			const int8_t price = secret % 10;
 
-			ranges::rotate(sequence, sequence.begin() + 1);
-			sequence[3] = static_cast<int16_t>(price - prev_price);
+			const auto diff = price - prev_price;
+			sequence = ((sequence << 5u) | (diff & 0x1F)) & 0xFFFFF;
 			prev_price = price;
 
-			if (i > 3 && seen.emplace(sequence).second)
+			if (i > 3 && !seen[sequence]) {
 				global_prices[sequence] += price;
+				seen[sequence] = 1;
+			}
 		}
 	}
 
-	const auto max_price = ranges::max(global_prices | views::values);
+	const auto max_price = ranges::max(global_prices);
 
 	const auto duration = duration_cast<microseconds>(high_resolution_clock::now() - start_time);
 	println("Day 22b: {} ({})", max_price, duration);
